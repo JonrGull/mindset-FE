9e82307465fcbca4aa27c39e8ce6910c7b0074ce
yccjs <yccjsalvador@gmail.com>
2022-07-08 18:03:51 +0900

    fix: auth redirection

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,5 @@ export default function ({ store, redirect }) {
   console.log(typeof store, typeof redirect);
   if (!store.state.user) {
     redirect("/login");
-  } else {
-    window.location = "/";
   }
 }
