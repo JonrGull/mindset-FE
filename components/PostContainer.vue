684ccc954aeec41a39a3245ae5f7895429359aef
Jon <25947147+JonrGull@users.noreply.github.com>
2022-07-13 10:19:39 +0900

    refactor: post container component

---
+++ components/PostContainer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="posts-container">
+      <div v-if="posts.length > 0">No posts</div>
+
+      <div v-else="post in posts.slice().reverse()" :key="post.id">
+        {{ post.text }}
+        {{ post.createdAt }}
+        {{ console.log(`posts`, post) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostsContainer",
+  props: ["posts"],
+};
+</script>
+
+<style scoped></style>
